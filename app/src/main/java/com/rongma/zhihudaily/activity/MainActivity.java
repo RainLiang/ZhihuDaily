7bd29c8afde4f0f7694688ac3159e0038b876c88
RainLiang <rongma.dev@gmail.com>
2016-04-21 23:56:42 +0800

    0420

--- app/src/main/java/com/rongma/zhihudaily/activity/MainActivity.java
+++ app/src/main/java/com/rongma/zhihudaily/activity/MainActivity.java
@@ -2,13 +2,18 @@ package com.rongma.zhihudaily.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 
 import com.rongma.zhihudaily.R;
 import com.rongma.zhihudaily.fragment.MainPageFragment;
 import com.rongma.zhihudaily.global.BaseFragmentActivity;
 
 public class MainActivity extends BaseFragmentActivity {
-
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle; //在Toolbar左侧显示的按钮,可以弹出DrawerLayout
+    private DrawerLayout drawer;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,14 @@ public class MainActivity extends BaseFragmentActivity {
     @Override
     protected void bindViews(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toolbar.setTitle("首页");
+        setSupportActionBar(toolbar);
+        //设置DrawerToggle
+        toggle = new ActionBarDrawerToggle(this,drawer,toolbar,0,0);
+        toggle.syncState();
+        drawer.setDrawerListener(toggle);
         MainPageFragment mainFrag = new MainPageFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.main_frag, mainFrag);
--- app/src/main/java/com/rongma/zhihudaily/global/BaseFragmentActivity.java
+++ app/src/main/java/com/rongma/zhihudaily/global/BaseFragmentActivity.java
@@ -1,14 +1,13 @@
 package com.rongma.zhihudaily.global;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 /**
  * Created by RongMa on 16/4/20.
  */
-public abstract class BaseFragmentActivity extends FragmentActivity
+public abstract class BaseFragmentActivity extends AppCompatActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
